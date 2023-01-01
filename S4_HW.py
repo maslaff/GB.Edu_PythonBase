f4f788312118c63164e6814156b18f331cc45191
MaSlaff <maslaff@yandex.ru>
2023-01-01 18:45:01 +0300

    s4hw B solved

--- S4_HW.py
+++ S4_HW.py
@@ -2,6 +2,13 @@
 # Methods. Здесь блок общих методов
 from random import randint as rnd
 
+file_a = 'file_a'
+file_b = 'file_b'
+file_sum = 'file_sum'
+len_poly = 14
+
+powchar_list = '⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ⁺ ⁻ ⁼ ⁽ ⁾ ⁿ ⁱ'.split()
+
 
 def get_num(msg):
     while True:
@@ -25,11 +32,10 @@ def get_rand_num(nonzero=False):
 # если k = 2, то многочлены могут быть => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
 
+# Преобразует целые числа в (юникод) надстрочные
 def powch(num):
     if num < 2:
         return ''
-
-    powchar_list = '⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ⁺ ⁻ ⁼ ⁽ ⁾ ⁿ ⁱ'.split()
 
     if num < 10:
         return powchar_list[num]
@@ -45,8 +51,8 @@ def polynom_to_str(k, verbose=False):
     polynom_str = ''
     pw = k
     while pw >= 0:
-        # koef = rnd(-100, 100)
-        koef = rnd(-3, 3)
+        koef = rnd(-100, 100)
+        # koef = rnd(-3, 3)
         # print(koef, end='    ')
 
         if pw == k:
@@ -68,26 +74,72 @@ def polynom_to_str(k, verbose=False):
     return polynom_str.replace(' + -', ' - ') + ' = 0'
 
 
-# print('\n', polynom_to_str(14))
+# print('\n', polynom_to_str(len_poly))
 
-file_a = open('file_a', 'w')
-file_b = open('file_b', 'w')
+def str_to_files():
+    file_a = open(file_a, 'w')
+    file_b = open(file_b, 'w')
 
-file_a.write(polynom_to_str(14, verbose=True))
-file_b.write(polynom_to_str(14, verbose=True))
+    file_a.write(polynom_to_str(14))
+    file_b.write(polynom_to_str(14))
 
-file_a.close()
-file_b.close()
+    file_a.close()
+    file_b.close()
+
+
+# str_to_files()
 
 # B. Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
 
+# Возвращает первую строку файла
+def read_str_from_file(file_):
+    f = open(file_)
+    _string = f.readline()
+    f.close()
+    return _string
 
-def sum_two_polynom_str():
-    pass
+
+# Преобразует (юникод) надстрочные числа в целые
+def powch_to_num(powch_):
+    if powch_ == '':
+        return 1
+
+    num = ''
+    for i in powch_:
+        num += str(powchar_list.index(i))
+    return int(num)
 
 
-# sum_two_polynom_str()
+# Разбирает строку на члены, возвращает словарь
+def parse_poly(_string: str):
+    poly_dict = {}
+    poly_arr = _string.replace(' ', '').replace(
+        '-', ' -').replace('+', ' ').replace('=0', '').lower().split()
+
+    for i in poly_arr:
+        print(i, end=' ')
+        rec = i.split('x')
+        ky = powch_to_num(rec[1]) if len(rec) > 1 else 0
+        poly_dict[ky] = int(rec[0])
+
+    print('\n', poly_dict)
+    return poly_dict
+
+
+# Складывает (сумма) значения двух словарей
+def sum_two_polynom(k):
+    poly_a: dict = parse_poly(read_str_from_file(file_a))
+    poly_b: dict = parse_poly(read_str_from_file(file_b))
+    poly_sum = {}
+
+    for i in range(k+1):
+        poly_sum[i] = poly_a.get(i, 0) + poly_b.get(i, 0)
+
+    return poly_sum
+
+
+print(sum_two_polynom(len_poly))
 
 # НЕОБЯЗАТЕЛЬНОЕ, ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ:
 # Расширить значение коэффициентов до [-100..100]
