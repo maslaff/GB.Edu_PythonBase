374a283c389feff3a8c7c680ecf22154cbb5fce9
MaSlaff <maslaff@yandex.ru>
2023-01-01 20:29:46 +0300

    s4hw solved

--- S4_HW.py
+++ S4_HW.py
@@ -9,21 +9,6 @@ len_poly = 14
 
 powchar_list = '⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ⁺ ⁻ ⁼ ⁽ ⁾ ⁿ ⁱ'.split()
 
-
-def get_num(msg):
-    while True:
-        try:
-            val = float(input(msg))
-        except ValueError:
-            print("Требуется число")
-        else:
-            return val
-
-
-def get_rand_num(nonzero=False):
-    while True:
-        pass
-
 # =============================
 
 # A. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и
@@ -47,50 +32,58 @@ def powch(num):
     return pw
 
 
-def polynom_to_str(k, verbose=False):
-    polynom_str = ''
+# Возвращает словарь коэффициентов для степеней до k
+def rand_koefs(k):
+    polynom_dict = {}
     pw = k
     while pw >= 0:
         koef = rnd(-100, 100)
         # koef = rnd(-3, 3)
-        # print(koef, end='    ')
 
         if pw == k:
             if not koef:
                 continue
 
+        polynom_dict[pw] = koef
+
+        pw -= 1
+    return polynom_dict
+
+
+# принимает словарь, возвращает многочлен ввиде строки
+def polynom_to_str(polydict: dict, verbose=False):
+    polynom_str = ''
+    for powr, koef in polydict.items():
         if koef:
-            if pw != k:
+            if powr != max(polydict.keys()):
                 polynom_str += ' + '
-            if koef > 1 or koef < -1 or pw == 0:
+            if koef > 1 or koef < -1 or powr == 0:
                 polynom_str += str(koef)
             elif koef == -1:
                 polynom_str += '-'
 
-            if pw:
-                polynom_str += f"x{powch(pw)}" if not verbose else f"*x**{pw}"
-        pw -= 1
+            if powr:
+                polynom_str += f"x{powch(powr)}" if not verbose else f"*x**{powr}"
 
     return polynom_str.replace(' + -', ' - ') + ' = 0'
 
 
-# print('\n', polynom_to_str(len_poly))
+# print('\n', polynom_to_str(rand_koefs(len_poly)))
+
 
 def str_to_files():
-    file_a = open(file_a, 'w')
-    file_b = open(file_b, 'w')
+    fa = open(file_a, 'w')
+    fb = open(file_b, 'w')
 
-    file_a.write(polynom_to_str(14))
-    file_b.write(polynom_to_str(14))
+    fa.write(polynom_to_str(rand_koefs(len_poly)))
+    fb.write(polynom_to_str(rand_koefs(len_poly)))
 
-    file_a.close()
-    file_b.close()
-
-
-# str_to_files()
+    fa.close()
+    fb.close()
 
 # B. Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
+
 
 # Возвращает первую строку файла
 def read_str_from_file(file_):
@@ -118,28 +111,49 @@ def parse_poly(_string: str):
         '-', ' -').replace('+', ' ').replace('=0', '').lower().split()
 
     for i in poly_arr:
-        print(i, end=' ')
         rec = i.split('x')
         ky = powch_to_num(rec[1]) if len(rec) > 1 else 0
-        poly_dict[ky] = int(rec[0])
+        vl = int(rec[0] if rec[0] and rec[0] != '-' else rec[0] + '1')
+        poly_dict[ky] = vl
 
-    print('\n', poly_dict)
+    # print('\n', poly_dict)
     return poly_dict
 
 
 # Складывает (сумма) значения двух словарей
-def sum_two_polynom(k):
+def sum_two_polynom(file_a, file_b):
     poly_a: dict = parse_poly(read_str_from_file(file_a))
     poly_b: dict = parse_poly(read_str_from_file(file_b))
+    k = max(*poly_a.keys(), *poly_b.keys())
+
     poly_sum = {}
 
-    for i in range(k+1):
+    for i in range(k, -1, -1):
         poly_sum[i] = poly_a.get(i, 0) + poly_b.get(i, 0)
 
     return poly_sum
 
 
-print(sum_two_polynom(len_poly))
+str_to_files()
+poly_str_sum = polynom_to_str(sum_two_polynom(file_a, file_b))
+
+
+f = open(file_sum, 'w')
+# print(poly_str_sum)
+f.write(poly_str_sum)
+f.close()
+
+fa = open(file_a)
+fb = open(file_b)
+fsum = open(file_sum)
+
+print(fa.readline())
+print(fb.readline())
+print(fsum.readline())
+
+fa.close()
+fb.close()
+fsum.close()
 
 # НЕОБЯЗАТЕЛЬНОЕ, ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ:
 # Расширить значение коэффициентов до [-100..100]
